feaa2fcee5ade6ec4507396d85f1db1ba4baa66b
tomohiro.kojima <niniseseko@gmail.com>
2021-09-10 12:35:00 +0000

    投稿機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'posts/create'
+  get 'posts/destroy'
   root to: 'toppages#index'
   
   get 'login', to: 'sessions#new'
@@ -7,4 +9,5 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   resources :users, only: [:show, :create]
+  resources :posts, only: [:new, :create, :destroy]
 end
