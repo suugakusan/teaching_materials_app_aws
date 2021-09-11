6ca84b2ec0c7f382beb502f9967f36760491938b
tomohiro.kojima <niniseseko@gmail.com>
2021-09-11 05:16:11 +0000

    投稿編集機能まだ途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'favorites/create'
-  get 'favorites/destroy'
   root to: 'toppages#index'
   
   get 'login', to: 'sessions#new'
@@ -16,6 +14,6 @@ Rails.application.routes.draw do
     end
   end
   resources :favorites, only: [:create, :destroy]
-  resources :posts, only: [:new, :create, :destroy]
+  resources :posts, only: [:new, :create, :destroy, :update, :edit]
   resources :relationships, only: [:create, :destroy]
 end
