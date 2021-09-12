396f13ef070b82d09af048b1335c028249b232cb
tomohiro.kojima <niniseseko@gmail.com>
2021-09-12 05:48:57 +0000

    教科のフォーム完成

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,19 +8,20 @@ class Post < ApplicationRecord
   validates :content, presence: true, length: { maximum: 255 }
   validates :title, presence: true, length: { maximum: 255 }
   validates :file, presence: true, length: { maximum: 255 }
-  validates :subject_id, presence: true, length: { maximum: 1 }
+  validates :subject_id, presence: true
  
- #enum subject_id: {
-  #  国語: 1,
-   # 社会: 2,
-    #数学: 3,
-    #理科: 4,
-    #英語: 5,
-    #美術: 6,
-    #保体: 7,
-    #技家: 8,
-    #音楽: 9
-  #}
+ enum subject_id: {
+    国語: 1,
+    社会: 2,
+    数学: 3,
+    理科: 4,
+    英語: 5,
+    美術: 6,
+    保体: 7,
+    技家: 8,
+    音楽: 9
+  }
+
 
 
   def self.search(keyword)
