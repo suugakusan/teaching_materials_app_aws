d8c5457329e0919d620501a19d4b1267e3aa16d1
tomohiro.kojima <niniseseko@gmail.com>
2021-09-12 04:55:17 +0000

    教科用の検索フォーム

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,8 +9,25 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
   validates :file, presence: true, length: { maximum: 255 }
   validates :subject_id, presence: true, length: { maximum: 1 }
-  
+ 
+ #enum subject_id: {
+  #  国語: 1,
+   # 社会: 2,
+    #数学: 3,
+    #理科: 4,
+    #英語: 5,
+    #美術: 6,
+    #保体: 7,
+    #技家: 8,
+    #音楽: 9
+  #}
+
+
   def self.search(keyword)
-     where(["title like? OR content like? OR subject_id like?", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%"])
+     where(["title like? OR content like?", "%#{keyword}%", "%#{keyword}%"])
+  end
+  
+  def self.subject_search(keyword)
+     where(["subject_id like?", "%#{keyword}%"])
   end
 end
