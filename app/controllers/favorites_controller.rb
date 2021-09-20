f8d1e152c7bee45e136b24a3f874f0c9224e7913
tomohiro.kojima <niniseseko@gmail.com>
2021-09-20 05:44:25 +0000

    非同期更新しないとダメ

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,14 +4,10 @@ class FavoritesController < ApplicationController
   def create
     post = Post.find(params[:post_id])
     current_user.favorite(post)
-    flash[:success] = '投稿をお気に入りしました。'
-    redirect_to request.referer
   end
 
   def destroy
     post = Post.find(params[:post_id])
     current_user.unfavorite(post)
-    flash[:success] = '投稿のお気に入りを解除しました。'
-    redirect_to request.referer
   end
 end
