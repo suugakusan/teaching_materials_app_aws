e75c402f252396cbe047759b26403caad3569555
tomohiro.kojima <niniseseko@gmail.com>
2021-09-20 10:27:42 +0000

    非同期のファイルを分割

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,12 +2,12 @@ class FavoritesController < ApplicationController
   before_action :require_user_logged_in
   
   def create
-    post = Post.find(params[:post_id])
-    current_user.favorite(post)
+    @post = Post.find(params[:post_id])
+    current_user.favorite(@post)
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    current_user.unfavorite(post)
+    @post = Post.find(params[:post_id])
+    current_user.unfavorite(@post)
   end
 end
