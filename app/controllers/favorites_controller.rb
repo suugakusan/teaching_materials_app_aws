0a6a59ecb547e5af2aa2e80d59c051837e3d7c35
tomohiro.kojima <niniseseko@gmail.com>
2021-09-21 13:18:35 +0000

    お気に入り一覧の非同期ok

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,11 +2,13 @@ class FavoritesController < ApplicationController
   before_action :require_user_logged_in
   
   def create
+    @favorite = Post.find(params[:post_id])
     @post = Post.find(params[:post_id])
     current_user.favorite(@post)
   end
 
   def destroy
+     @favorite = Post.find(params[:post_id])
     @post = Post.find(params[:post_id])
     current_user.unfavorite(@post)
   end
