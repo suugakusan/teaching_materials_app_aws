0f9c68c848a461a2e34e4584165c755e3d9486f0
tomohiro.kojima <niniseseko@gmail.com>
2021-10-07 10:51:16 +0000

    enumの共通化と検索はID

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,25 +10,12 @@ class Post < ApplicationRecord
   validates :file, presence: true
   validates :subject_id, presence: true
  
- enum subject_id: {
-    国語: 1,
-    社会: 2,
-    数学: 3,
-    理科: 4,
-    英語: 5,
-    音楽: 6,
-    保体: 7,
-    技家: 8,
-    美術: 9
-  }
-
-
-
+ 
   def self.search(keyword)
      where(["title like? OR content like?", "%#{keyword}%", "%#{keyword}%"])
   end
   
-  def self.subject_search(keyword)
-     where(["subject_id::text like?", "%#{keyword}%"])
+  def self.subject_search(keyword1)
+     where(subject_id: keyword1)
   end
 end
