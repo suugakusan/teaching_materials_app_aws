3949e1573d187a61a634916e561117dda4ba6475
tomohiro.kojima <niniseseko@gmail.com>
2021-10-08 10:39:13 +0000

    postにinclude

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,7 +2,7 @@ class ToppagesController < ApplicationController
   def index
     if logged_in?
       @post = current_user.posts.build
-      @pagy, @posts = pagy(Post.order(id: :desc))
+      @pagy, @posts = pagy(Post.includes(:user).order(id: :desc))
       @user = current_user
       @pagy, @favoritings = pagy(@user.favoritings)
     end
