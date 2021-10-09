718a52cf92c0f958b89b329551fa36ded89b03b1
tomohiro.kojima <niniseseko@gmail.com>
2021-10-09 09:29:14 +0000

    アイコンの分岐未完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image, :prefecture_id, :school_id, :profile)
   end
   
 end
