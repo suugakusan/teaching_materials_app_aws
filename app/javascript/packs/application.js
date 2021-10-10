549e800d8970fd87b254521c9930e1744502a413
tomohiro.kojima <niniseseko@gmail.com>
2021-10-10 12:42:11 +0000

    詳細画面に教科書が表示される

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,7 +21,6 @@ Turbolinks.start()
 $(function() {
   $('#school_type').change(function() {
     let text = $("#school_type option:selected").val();
-    
     if ( text == "小学校" ){
       $('#post_grade').children().remove();
       $('#school_grade').fadeIn();
@@ -33,6 +32,19 @@ $(function() {
       $('#post_grade').append($('<option>').html("４年").val("４年"));
       $('#post_grade').append($('<option>').html("５年").val("５年"));
       $('#post_grade').append($('<option>').html("６年").val("６年"));
+      $('#post_subject_id').children().remove();
+      $('#subject_select').fadeIn();
+      $('#post_subject_id').append($('<option>').html("未選択").val("未選択"));
+      $('#post_subject_id').append($('<option>').html("国語").val("国語"));
+      $('#post_subject_id').append($('<option>').html("社会").val("社会"));
+      $('#post_subject_id').append($('<option>').html("算数").val("算数"));
+      $('#post_subject_id').append($('<option>').html("理科").val("理科"));
+      $('#post_subject_id').append($('<option>').html("英語").val("英語"));
+      $('#post_subject_id').append($('<option>').html("音楽").val("音楽"));
+      $('#post_subject_id').append($('<option>').html("保体").val("保体"));
+      $('#post_subject_id').append($('<option>').html("家庭").val("家庭"));
+      $('#post_subject_id').append($('<option>').html("生活").val("生活"));
+      $('#post_subject_id').append($('<option>').html("図画工作").val("図画工作"));
     } else if( text == "中学校") {
       $('#post_grade').children().remove();
       $('#school_grade').fadeIn();
@@ -41,6 +53,18 @@ $(function() {
       $('#post_grade').append($('<option>').html("１年").val("１年"));
       $('#post_grade').append($('<option>').html("２年").val("２年"));
       $('#post_grade').append($('<option>').html("３年").val("３年"));
+      $('#post_subject_id').children().remove();
+      $('#subject_select').fadeIn();
+      $('#post_subject_id').append($('<option>').html("未選択").val("未選択"));
+      $('#post_subject_id').append($('<option>').html("国語").val("国語"));
+      $('#post_subject_id').append($('<option>').html("社会").val("社会"));
+      $('#post_subject_id').append($('<option>').html("数学").val("数学"));
+      $('#post_subject_id').append($('<option>').html("理科").val("理科"));
+      $('#post_subject_id').append($('<option>').html("英語").val("英語"));
+      $('#post_subject_id').append($('<option>').html("音楽").val("音楽"));
+      $('#post_subject_id').append($('<option>').html("保体").val("保体"));
+      $('#post_subject_id').append($('<option>').html("美術").val("美術"));
+      $('#post_subject_id').append($('<option>').html("技家").val("技家"));
     } else if( text == "高校") {
       $('#post_grade').children().remove();
       $('#school_grade').fadeIn();
@@ -49,8 +73,22 @@ $(function() {
       $('#post_grade').append($('<option>').html("１年").val("１年"));
       $('#post_grade').append($('<option>').html("２年").val("２年"));
       $('#post_grade').append($('<option>').html("３年").val("３年"));
+      $('#post_subject_id').children().remove();
+      $('#subject_select').fadeIn();
+      $('#post_subject_id').append($('<option>').html("未選択").val("未選択"));
+      $('#post_subject_id').append($('<option>').html("国語").val("国語"));
+      $('#post_subject_id').append($('<option>').html("社会").val("社会"));
+      $('#post_subject_id').append($('<option>').html("数学").val("数学"));
+      $('#post_subject_id').append($('<option>').html("理科").val("理科"));
+      $('#post_subject_id').append($('<option>').html("英語").val("英語"));
+      $('#post_subject_id').append($('<option>').html("音楽").val("音楽"));
+      $('#post_subject_id').append($('<option>').html("保体").val("保体"));
+      $('#post_subject_id').append($('<option>').html("美術").val("美術"));
+      $('#post_subject_id').append($('<option>').html("技家").val("技家"));
     } else if( text == "未選択") {
       $('#school_grade').fadeOut();
+      $('#subject_select').fadeOut();
+      $('#subjectbook').fadeOut();
     }
   });
 
@@ -61,80 +99,118 @@ $(function() {
   $(document).on('change', '#subject_select', function() {
     let subjecttext = $("#subject_select  option:selected").val();
     if ( subjecttext == "国語"){
-    　$('.subjectbook').hide();
-    　$('#school_type').append('#japanesebooks');
-      $('#japanesebooks').fadeIn();
+      $('#post_schoolbook').children().remove();
+      $('#subjectbook').fadeIn();
+      $('#post_schoolbook').append($('<option>').html("未選択").val("未選択"));
+      $('#post_schoolbook').append($('<option>').html("東京書籍").val("東京書籍"));
+      $('#post_schoolbook').append($('<option>').html("三省堂").val("三省堂"));
+      $('#post_schoolbook').append($('<option>').html("教育出版").val("教育出版"));
+      $('#post_schoolbook').append($('<option>').html("光村図書出版").val("光村図書出版"));
     } else if ( subjecttext == "社会") {
-      $('.subjectbook').hide();
-      $('#societybooks').fadeIn();
+      $('#post_schoolbook').children().remove();
+      $('#subjectbook').fadeIn();
+      $('#post_schoolbook').append($('<option>').html("未選択").val("未選択"));
+      $('#post_schoolbook').append($('<option>').html("東京書籍").val("東京書籍"));
+      $('#post_schoolbook').append($('<option>').html("教育出版").val("教育出版"));
+      $('#post_schoolbook').append($('<option>').html("帝国書院").val("帝国書院"));
+      $('#post_schoolbook').append($('<option>').html("山川出版社").val("山川出版社"));
+      $('#post_schoolbook').append($('<option>').html("日本文教出版").val("日本文教出版"));
+      $('#post_schoolbook').append($('<option>').html("自由社").val("自由社"));
+      $('#post_schoolbook').append($('<option>').html("育鵬社").val("育鵬社"));
+      $('#post_schoolbook').append($('<option>').html("清水書院").val("清水書院"));
     } else if ( subjecttext == "理科") {
-      $('.subjectbook').hide();
-      $('#sciencebooks').fadeIn();
+      $('#post_schoolbook').children().remove();
+      $('#subjectbook').fadeIn();
+      $('#post_schoolbook').append($('<option>').html("未選択").val("未選択"));
+      $('#post_schoolbook').append($('<option>').html("東京書籍").val("東京書籍"));
+      $('#post_schoolbook').append($('<option>').html("教育出版").val("教育出版"));
+      $('#post_schoolbook').append($('<option>').html("大日本図書").val("大日本図書"));
+      $('#post_schoolbook').append($('<option>').html("学校図書").val("学校図書"));
+      $('#post_schoolbook').append($('<option>').html("啓林館").val("啓林館"));
     } else if ( subjecttext == "音楽") {
-      $('.subjectbook').hide();
-      $('#musicbooks').fadeIn();
+      $('#post_schoolbook').children().remove();
+      $('#subjectbook').fadeIn();
+      $('#post_schoolbook').append($('<option>').html("未選択").val("未選択"));
+      $('#post_schoolbook').append($('<option>').html("教育出版").val("教育出版"));
+      $('#post_schoolbook').append($('<option>').html("教育芸術社").val("教育芸術社"));
     } else if ( subjecttext == "美術") {
-      $('.subjectbook').hide();
-      $('#artbooks').fadeIn();
+      $('#post_schoolbook').children().remove();
+      $('#subjectbook').fadeIn();
+      $('#post_schoolbook').append($('<option>').html("未選択").val("未選択"));
+      $('#post_schoolbook').append($('<option>').html("光村図書出版").val("光村図書出版"));
+      $('#post_schoolbook').append($('<option>').html("日本文教出版").val("日本文教出版"));
+      $('#post_schoolbook').append($('<option>').html("開隆堂出版").val("開隆堂出版"));
     } else if ( subjecttext == "保体") {
-      $('.subjectbook').hide();
-      $('#pebooks').fadeIn();
+      $('#post_schoolbook').children().remove();
+      $('#subjectbook').fadeIn();
+      $('#post_schoolbook').append($('<option>').html("未選択").val("未選択"));
+      $('#post_schoolbook').append($('<option>').html("東京書籍").val("東京書籍"));
+      $('#post_schoolbook').append($('<option>').html("大日本図書").val("大日本図書"));
+      $('#post_schoolbook').append($('<option>').html("大修館書店").val("大修館書店"));
+      $('#post_schoolbook').append($('<option>').html("学研教育みらい").val("学研教育みらい"));
     } else if ( subjecttext == "英語") {
-      $('.subjectbook').hide();
-      $('#englishbooks').fadeIn();
+      $('#post_schoolbook').children().remove();
+      $('#subjectbook').fadeIn();
+      $('#post_schoolbook').append($('<option>').html("未選択").val("未選択"));
+      $('#post_schoolbook').append($('<option>').html("教育出版").val("教育出版"));
+      $('#post_schoolbook').append($('<option>').html("光村図書出版").val("光村図書出版"));
+      $('#post_schoolbook').append($('<option>').html("啓林館").val("啓林館"));
     } else if ( subjecttext == "生活") {
-      $('.subjectbook').hide();
-      $('#lifebooks').fadeIn();
-    } else if ( subjecttext == "未選択") {
-      $('.subjectbook').hide();
+      $('#post_schoolbook').children().remove();
+      $('#subjectbook').fadeIn();
+      $('#post_schoolbook').append($('<option>').html("未選択").val("未選択"));
+      $('#post_schoolbook').append($('<option>').html("東京書籍").val("東京書籍"));
+      $('#post_schoolbook').append($('<option>').html("教育出版").val("教育出版"));
+      $('#post_schoolbook').append($('<option>').html("光村図書出版").val("光村図書出版"));
+      $('#post_schoolbook').append($('<option>').html("日本文教出版").val("日本文教出版"));
+      $('#post_schoolbook').append($('<option>').html("大日本図書").val("大日本図書"));
+      $('#post_schoolbook').append($('<option>').html("学校図書").val("学校図書"));
+      $('#post_schoolbook').append($('<option>').html("啓林館").val("啓林館"));
+      $('#post_schoolbook').append($('<option>').html("信州教育出版社").val("信州教育出版社"));
     } else if ( subjecttext == "数学") {
-      $('.subjectbook').hide();
-      $('#mathbooks').fadeIn();
+      $('#post_schoolbook').children().remove();
+      $('#subjectbook').fadeIn();
+      $('#post_schoolbook').append($('<option>').html("未選択").val("未選択"));
+      $('#post_schoolbook').append($('<option>').html("東京書籍").val("東京書籍"));
+      $('#post_schoolbook').append($('<option>').html("教育出版").val("教育出版"));
+      $('#post_schoolbook').append($('<option>').html("日本文教出版").val("日本文教出版"));
+      $('#post_schoolbook').append($('<option>').html("大日本図書").val("大日本図書"));
+      $('#post_schoolbook').append($('<option>').html("学校図書").val("学校図書"));
+      $('#post_schoolbook').append($('<option>').html("啓林館").val("啓林館"));
+      $('#post_schoolbook').append($('<option>').html("数研出版").val("数研出版"));
     } else if ( subjecttext == "技家") {
-      $('.subjectbook').hide();
-      $('#technologybooks').fadeIn();
-    }
-  });
-});
-
-$(function() {
-  $(document).on('change', '#elementary_subject_select', function() {
-    let subjecttext = $("#elementary_subject_select  option:selected").val();
-    if ( subjecttext == "国語"){
-    　$('.subjectbook').hide();
-      $('#japanesebooks').fadeIn();
-    } else if ( subjecttext == "社会") {
-      $('.subjectbook').hide();
-      $('#societybooks').fadeIn();
-    } else if ( subjecttext == "算数") {
-      $('.subjectbook').hide();
-      $('#mathbooks').fadeIn();
-    } else if ( subjecttext == "理科") {
-      $('.subjectbook').hide();
-      $('#sciencebooks').fadeIn();
-    } else if ( subjecttext == "音楽") {
-      $('.subjectbook').hide();
-      $('#musicbooks').fadeIn();
-    } else if ( subjecttext == "美術") {
-      $('.subjectbook').hide();
-      $('#artbooks').fadeIn();
-    } else if ( subjecttext == "保体") {
-      $('.subjectbook').hide();
-      $('#pebooks').fadeIn();
-    } else if ( subjecttext == "英語") {
-      $('.subjectbook').hide();
-      $('#englishbooks').fadeIn();
-    } else if ( subjecttext == "生活") {
-      $('.subjectbook').hide();
-      $('#lifebooks').fadeIn();
+      $('#post_schoolbook').children().remove();
+      $('#subjectbook').fadeIn();
+      $('#post_schoolbook').append($('<option>').html("未選択").val("未選択"));
+      $('#post_schoolbook').append($('<option>').html("東京書籍").val("東京書籍"));
+      $('#post_schoolbook').append($('<option>').html("開隆堂出版").val("開隆堂出版"));
+      $('#post_schoolbook').append($('<option>').html("教育図書").val("教育図書"));
     } else if ( subjecttext == "図画工作") {
-      $('.subjectbook').hide();
-      $('#drawingbooks').fadeIn();
+      $('#post_schoolbook').children().remove();
+      $('#subjectbook').fadeIn();
+      $('#post_schoolbook').append($('<option>').html("未選択").val("未選択"));
+      $('#post_schoolbook').append($('<option>').html("日本文教出版").val("日本文教出版"));
+      $('#post_schoolbook').append($('<option>').html("開隆堂出版").val("開隆堂出版"));
     } else if ( subjecttext == "家庭") {
-      $('.subjectbook').hide();
-      $('#homebooks').fadeIn();
+      $('#post_schoolbook').children().remove();
+      $('#subjectbook').fadeIn();
+      $('#post_schoolbook').append($('<option>').html("未選択").val("未選択"));
+      $('#post_schoolbook').append($('<option>').html("東京書籍").val("東京書籍"));
+      $('#post_schoolbook').append($('<option>').html("開隆堂出版").val("開隆堂出版"));
+    } else if ( subjecttext == "算数") {
+      $('#post_schoolbook').children().remove();
+      $('#subjectbook').fadeIn();
+      $('#post_schoolbook').append($('<option>').html("未選択").val("未選択"));
+      $('#post_schoolbook').append($('<option>').html("東京書籍").val("東京書籍"));
+      $('#post_schoolbook').append($('<option>').html("教育出版").val("教育出版"));
+      $('#post_schoolbook').append($('<option>').html("日本文教出版").val("日本文教出版"));
+      $('#post_schoolbook').append($('<option>').html("大日本図書").val("大日本図書"));
+      $('#post_schoolbook').append($('<option>').html("学校図書").val("学校図書"));
+      $('#post_schoolbook').append($('<option>').html("啓林館").val("啓林館"));
+      $('#post_schoolbook').append($('<option>').html("数研出版").val("数研出版"));
     } else if ( subjecttext == "未選択") {
-      $('.subjectbook').hide();
+      $('#post_schoolbook').children().remove();
+      $('#subjectbook').fadeOut();
     }
   });
 });
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -37,34 +37,6 @@ class ApplicationRecord < ActiveRecord::Base
     図画工作: 13
   }, _suffix: true
   
-  
-  enum junior_subject_id: {
-    未選択: 0,
-    国語: 1,
-    社会: 2,
-    数学: 3,
-    理科: 4,
-    英語: 5,
-    音楽: 6,
-    保体: 7,
-    技家: 8,
-    美術: 9
-  }, _suffix: true
-  
-  enum elementary_subject_id: {
-    未選択: 0,
-    国語: 1,
-    社会: 2,
-    算数: 10,
-    理科: 4,
-    英語: 5,
-    音楽: 6,
-    保体: 7,
-    生活: 11,
-    家庭: 12,
-    図画工作: 13
-  }, _suffix: true
-  
   enum prefecture_id: {
     北海道: 1,
     青森県: 2,
@@ -121,102 +93,27 @@ class ApplicationRecord < ActiveRecord::Base
     高校: 3
   }, _suffix: true
   
-  enum japanesebook: {
-    未選択: 20,
-    東京書籍: 0,
-    三省堂: 1,
-    教育出版: 2,
-    光村図書出版: 3
-  }, _suffix: true
-  
-  enum societybook: {
-    未選択: 20,
-    東京書籍: 0,
-    教育出版: 2,
-    帝国書院: 4,
-    山川出版社: 5,
-    日本文教出版: 6,
-    自由社: 7,
-    育鵬社: 8,
-    清水書院: 9,
-  }, _suffix: true
-  
-  enum mathbook: {
-    未選択: 20,
-    東京書籍: 0,
-    大日本図書: 10,
-    学校図書: 11,
-    教育出版: 12,
+  enum schoolbook: {
+    未選択: 0,
+    東京書籍: 1,
+    三省堂: 2,
+    教育出版: 3,
+    光村図書出版: 4,
+    帝国書院: 5,
+    山川出版社: 6,
+    日本文教出版: 7,
+    自由社: 8,
+    育鵬社: 9,
+    清水書院: 10,
+    大日本図書: 11,
+    学校図書: 12,
     啓林館: 13,
     数研出版: 14,
-    日本文教出版: 6
-  }, _suffix: true
-  
-  enum sciencebook: {
-    未選択: 20,
-    東京書籍: 0,
-    日本図書: 10,
-    学校図書: 11,
-    教育出版: 12,
-    啓林館: 13,
-  }, _suffix: true
-  
-  enum musicbook: {
-    未選択: 20,
-    教育出版: 12,
-    教育芸術社: 15
-  }, _suffix: true
-  
-  enum artbook: {
-    未選択: 20,
+    教育芸術社: 15,
     開隆堂出版: 16,
-    光村図書出版: 3,
-    日本文教出版: 6
-  }, _suffix: true
-  
-  enum pebook: {
-    未選択: 20,
-    東京書籍: 0,
-    大日本図書: 10,
     大修館書店: 17,
-    学研教育みらい:18
-  }, _suffix: true
-  
-  enum technologybook: {
-    未選択: 20,
-    東京書籍: 0,
+    学研教育みらい:18,
     教育図書: 19,
-    開隆堂出版: 16,
-  }, _suffix: true
-  
-  enum englishbook: {
-    未選択: 20,
-    教育出版: 12,
-    光村図書出版: 3,
-    啓林館: 13
-  }, _suffix: true
-  
-  enum lifebook: {
-    未選択: 20,
-    東京書籍: 0,
-    大日本図書: 10,
-    学校図書: 11,
-    教育出版: 12,
-    信州教育出版社: 21,
-    光村図書出版: 3,
-    啓林館: 13,
-    日本文教出版: 6
-  }, _suffix: true
-  
-  enum drawingbook: {
-    未選択: 20,
-    開隆堂出版: 16,
-    日本文教出版: 6
-  }, _suffix: true
-  
-  enum homebook: {
-    未選択: 20,
-    東京書籍: 0,
-    開隆堂出版: 16,
+    信州教育出版社: 20
   }, _suffix: true
 end
