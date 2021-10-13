c01da525d0f989c3689d964f33b986bb0d5b7f3a
tomohiro.kojima <niniseseko@gmail.com>
2021-10-13 20:40:31 +0000

    バリデーションの追加

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ApplicationRecord
   mount_uploader :comment_file, ImageUploader
   belongs_to :user
   belongs_to :post
+  validates :comment_content, presence: true, length: { maximum: 255 }
 end
