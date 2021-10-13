46c689ccc1fa08e93422254decd12a4ec56d23cb
tomohiro.kojima <niniseseko@gmail.com>
2021-10-13 10:24:33 +0000

    削除後の画面の修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,7 +26,7 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     flash[:success] = 'メッセージを削除しました。'
-    redirect_back(fallback_location: root_path)
+    redirect_to root_url
   end
   
   def edit
