c16482d94f5ef80ebe253f27c1067484e659b1d8
tomohiro.kojima <niniseseko@gmail.com>
2021-10-18 10:30:29 +0000

    commentとpostを管理に追加

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -5,7 +5,7 @@ ActiveAdmin.setup do |config|
   # for each of the active admin pages.
   #
   config.site_title = "Teaching Materials App"
-
+  config.comments_registration_name = 'AdminComment'
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
