b4ebec03596d8a683e983cbbfedffb6015c9cda5
tomohiro.kojima <niniseseko@gmail.com>
2021-10-19 11:17:21 +0000

    groupの追加

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,18 +1,48 @@
 ActiveAdmin.register User do
-  permit_params :name, :image, :profile, :prefecture_id, :school_id
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name, :email, :password_digest, :image, :profile, :prefecture_id, :school_id, :schooltype, :grade, :subject_id, :schoolbook
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :email, :password_digest, :image, :profile, :prefecture_id, :school_id, :schooltype, :grade, :subject_id, :schoolbook]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  permit_params :name, :email, :password, :password_confirmation, :image, :prefecture_id, :school_id, :profile, :group
+  
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :email
+    column :image
+    column :prefecture_id
+    column :school_id
+    column :profile
+    column :group
+    column :current_sign_in_at
+    column :sign_in_count
+    column :created_at
+    actions
+  end
+
+  filter :name
+  filter :email
+  filter :image
+  filter :prefecture_id
+  filter :school_id
+  filter :profile
+  filter :group
+  filter :current_sign_in_at
+  filter :sign_in_count
+  filter :created_at
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+      f.input :image
+      f.input :prefecture_id
+      f.input :school_id
+      f.input :profile
+      f.input :group
+    end
+    f.actions
+  end
+  
+  
   
 end
