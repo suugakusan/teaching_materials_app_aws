652a5ad6bf0bc1e2bb4691388f7202996c36e52e
tomohiro.kojima <niniseseko@gmail.com>
2021-10-19 11:59:08 +0000

    パスワードなしに更新

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 4 }
+  validates :password, presence: true, length: { minimum: 4 }, on: :create
   has_secure_password
   
   has_many :posts
