b3069cc06cd18709c2c3e34bc1087f29bb9f9629
tomohiro.kojima <niniseseko@gmail.com>
2021-10-23 05:31:10 +0000

    お問い合わせ用のメイラーを設定

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,14 +1,20 @@
 class ContactMailer < ApplicationMailer
 
-  def user_email(name:, email:)
-    @name = name
-    mail(
-      to: email,
-      subject: "【アプリ名】お問い合わせを受付いたしました"
-    )
+  ADMIN_EMAIL = "teachingmatrialsapp@gmail.com"
+
+  def user_email(contact)
+    @contact = contact
+    @name = contact.name.present? ? contact.name : contact.email
+    subject = "【教材シェア】お問い合わせを受付いたしました"
+
+    mail(to: contact.email, subject: subject)
   end
 
-  def admin_email
+  def admin_email(contact)
+    @contact = contact
+    @name = contact.name.present? ? contact.name : contact.email
+    subject = "【教材シェア】お問い合わせがありました"
 
+    mail(to: ADMIN_EMAIL, subject: subject)
   end
 end
