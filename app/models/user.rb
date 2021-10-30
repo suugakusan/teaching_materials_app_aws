632ddfbdbd49dc982bb5cb6f16f31e34496f8a45
tomohiro.kojima <niniseseko@gmail.com>
2021-10-30 11:38:44 +0000

    postのtitleテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   mount_uploader :image, ImageUploader
   before_save { self.email.downcase! }
-  validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 },
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :email, presence: true, length: { maximum: 30 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 4 }, on: :create
