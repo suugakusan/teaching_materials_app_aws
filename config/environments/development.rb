5a96479087831fe0d33b5ec6caf9908ea50d4940
tomohiro.kojima <niniseseko@gmail.com>
2021-10-30 07:53:49 +0000

    factoriesのファイル作成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,6 +7,7 @@ Rails.application.configure do
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+  config.factory_bot.definition_file_paths = ["spec/factories"]
 
   # Do not eager load code on boot.
   config.eager_load = false
