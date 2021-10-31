f260cd2e401090103d72799c9058a4bc1c9d0ea8
tomohiro.kojima <niniseseko@gmail.com>
2021-10-31 04:21:15 +0000

    comenntのcontentテスト

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -7,5 +7,31 @@ RSpec.describe Comment, type: :model do
         expect(subject).to eq true
       end
     end
+    context "comment_content が空のとき" do
+      let(:comment) { build(:comment, comment_content: "") }
+      it "エラーが発生する" do
+        expect(subject).to eq false
+        expect(comment.errors.messages[:comment_content]).to include "を入力してください"
+      end
+    end
+    context "comment_content が256文字以上のとき" do
+      let(:comment) { build(:comment, comment_content: "a" * 256) }
+      it "エラーが発生する" do
+        expect(subject).to eq false
+        expect(comment.errors.messages[:comment_content]).to include "は255文字以内で入力してください"
+      end
+    end
+    context "comment_content が255文字のとき" do
+      let(:comment) { build(:comment, comment_content: "a" * 255) }
+      it "保存できる" do
+        expect(subject).to eq true
+      end
+    end
+    context "comment_content が1文字のとき" do
+      let(:comment) { build(:comment, comment_content: "a" ) }
+      it "保存できる" do
+        expect(subject).to eq true
+      end
+    end
   end
 end
