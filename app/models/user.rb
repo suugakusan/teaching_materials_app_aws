9f4c2ab0a14568b0021ad3b480401bbee611cc20
tomohiro.kojima <niniseseko@gmail.com>
2021-10-31 04:31:00 +0000

    投稿が削除されたときのtest

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   mount_uploader :image, ImageUploader
   before_save { self.email.downcase! }
   validates :name, presence: true, length: { maximum: 20 }
-  validates :email, presence: true, length: { maximum: 30 },
+  validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 4 }, on: :create
